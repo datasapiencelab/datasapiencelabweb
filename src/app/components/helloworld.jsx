de2310fc8c15d3c06d132311dcf8e795e3d1a1e9
Sakib Ahamed Shahon <53832890+sakib3201@users.noreply.github.com>
2023-12-23 23:49:45 +0600

    Set up the initial tailwind components with DaisyUI and set a custom theme

---
+++ src/app/components/helloworld.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const HelloWorld =()=> {
+    return(
+        <h1 className="flex justify-center items-center text-xl h-14">Hello world, Just testing components</h1>
+    )
+}
+
+export default HelloWorld
---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import HelloWorld from './components/helloworld'
+
+export default function Home() {
+  return (
+      <main>
+        <HelloWorld/>
+      </main>
+  )
+}
