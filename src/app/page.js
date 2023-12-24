c6b208ac7c05ac544b5f302500778ae188525fab
Sakib Ahamed Shahon <53832890+sakib3201@users.noreply.github.com>
2023-12-24 17:56:55 +0600

    Added hero section

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import NavBar from './components/navbar'
 import Footer from './components/footer'
+import HomePageHeroSection from './components/herosection'
 export default function Home() {
   return (
       <main>
         <NavBar/>
+        <HomePageHeroSection/>
         <Footer/>
       </main>
   )
