2ef44fd467daf5598bd73febb9622bb70c83b4fa
Sakib Ahamed Shahon <53832890+sakib3201@users.noreply.github.com>
2023-12-29 23:44:46 +0600

    Minor improvement on footer

--- src/app/components/footer.jsx
+++ src/app/components/footer.jsx
@@ -8,24 +8,24 @@ const Footer = () =>{
         <img className="mask mask-circle" src={Logo.src} alt="Data Sapience Lab Logo" />
         </aside> 
         <nav>
-          <header className="footer-title">Services</header> 
-          <a className="link link-hover">Branding</a>
-          <a className="link link-hover">Design</a>
-          <a className="link link-hover">Marketing</a>
-          <a className="link link-hover">Advertisement</a>
+          <header className="footer-title underline">Services</header> 
+          <a className="link link-hover hover:text-secondary">Data Analytics & Visualization</a>
+          <a className="link link-hover hover:text-secondary">Machine Learning</a>
+          <a className="link link-hover hover:text-secondary">Mobile App</a>
+          <a className="link link-hover hover:text-secondary">Custom Website</a>
         </nav> 
         <nav>
-          <header className="footer-title">Company</header> 
-          <a className="link link-hover">About us</a>
-          <a className="link link-hover">Contact</a>
-          <a className="link link-hover">Jobs</a>
-          <a className="link link-hover">Press kit</a>
+          <header className="footer-title underline">Company</header> 
+          <a className="link link-hover hover:text-secondary">About us</a>
+          <a href="/contactus" className="link link-hover hover:text-secondary">Contact</a>
+          <a className="link link-hover hover:text-secondary">Career</a>
+          <a className="link link-hover hover:text-secondary">Press kit</a>
         </nav> 
         <nav>
-          <header className="footer-title">Legal</header> 
-          <a className="link link-hover">Terms of use</a>
-          <a className="link link-hover">Privacy policy</a>
-          <a className="link link-hover">Cookie policy</a>
+          <header className="footer-title underline">Legal</header> 
+          <a className="link link-hover hover:text-secondary">Terms of use</a>
+          <a className="link link-hover hover:text-secondary">Privacy policy</a>
+          <a className="link link-hover hover:text-secondary">Cookie policy</a>
         </nav>
       </footer>
     )
