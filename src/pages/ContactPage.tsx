e0816071a26881635d08466b138bca7b31a07495
tamjidhossen <tamjidhossen0x@gmail.com>
2026-01-04 18:23:28 +0600

    fix: add padding to ContactPage for improved layout

--- src/pages/ContactPage.tsx
+++ src/pages/ContactPage.tsx
@@ -3,7 +3,7 @@ import ContactInfo from "../components/sections/ContactInfo";
 
 export default function ContactPage() {
     return (
-        <div>
+        <div className="pt-16 md:pt-20 lg:pt-24">
             <ContactForm />
             <ContactInfo />
         </div>
