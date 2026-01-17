b63f1783150d3601d319056d350fdca991563f97
tamjidhossen <tamjidhossen0x@gmail.com>
2026-01-17 19:02:55 +0600

    feat: remove an empty className from the Spark component.

--- src/components/ui/Spark.tsx
+++ src/components/ui/Spark.tsx
@@ -6,7 +6,6 @@ export default function Spark() {
                 height="22"
                 viewBox="0 0 22 22"
                 fill="none"
-                className=""
             >
                 <path d="M11 0C11 6.07513 15.9249 11 22 11C15.9249 11 11 15.9249 11 22C11 15.9249 6.07513 11 0 11C6.07513 11 11 6.07513 11 0Z"
                     fill="currentColor"
